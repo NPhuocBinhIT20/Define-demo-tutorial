6db1acf727b5fc4a430c79c028cbe29bc1b6e9d3
unknown <binhnguyen110120@gmail.com>
2023-06-06 17:26:02 +0700

    tutorial

--- tutorial/src/App.tsx
+++ tutorial/src/App.tsx
@@ -1,75 +1,87 @@
-import { Refine } from "@refinedev/core";
 import {
-    notificationProvider,
-    ErrorComponent,
-    ThemedLayout,
+  ErrorComponent,
+  ThemedLayoutV2,
+  RefineThemes,
+  notificationProvider,
 } from "@refinedev/antd";
-import routerProvider, { NavigateToResource } from "@refinedev/react-router-v6";
+import { Refine } from "@refinedev/core";
+import routerBindings, {
+  NavigateToResource,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
 
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 
-import { AntdInferencer } from "@refinedev/inferencer/antd";
+import { BlogPostEdit } from "pages/blog-posts/edit";
+import { BlogPostList } from "pages/blog-posts/list";
+import { BlogPostShow } from "pages/blog-posts/show";
+import { BlogPostCreate } from "pages/blog-posts/create";
 
+import { ConfigProvider } from "antd";
 import "@refinedev/antd/dist/reset.css";
 
 const App: React.FC = () => {
-    return (
-        <BrowserRouter>
-            <Refine
-                routerProvider={routerProvider}
-                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-                notificationProvider={notificationProvider}
-                resources={[
-                    {
-                        name: "blog_posts",
-                        list: "/blog-posts",
-                        show: "/blog-posts/show/:id",
-                        create: "/blog-posts/create",
-                        edit: "/blog-posts/edit/:id",
-                        meta: { canDelete: true },
-                    },
-                    {
-                        name: "categories",
-                        list: "/categories",
-                        show: "/categories/show/:id",
-                    },
-                ]}
-            >
-                <Routes>
-                    <Route
-                        element={
-                            <ThemedLayout>
-                                <Outlet />
-                            </ThemedLayout>
-                        }
-                    >
-                        <Route index element={<NavigateToResource />} />
-                        <Route path="blog-posts">
-                            <Route index element={<AntdInferencer />} />
-                            <Route
-                                path="show/:id"
-                                element={<AntdInferencer />}
-                            />
-                            <Route path="create" element={<AntdInferencer />} />
-                            <Route
-                                path="edit/:id"
-                                element={<AntdInferencer />}
-                            />
-                        </Route>
-                        <Route path="categories">
-                            <Route index element={<AntdInferencer />} />
-                            <Route
-                                path="show/:id"
-                                element={<AntdInferencer />}
-                            />
-                        </Route>
-                        <Route path="*" element={<ErrorComponent />} />
-                    </Route>
-                </Routes>
-            </Refine>
-        </BrowserRouter>
-    );
-};
+  return (
+      <BrowserRouter>
+          <ConfigProvider theme={RefineThemes.Blue}>
+              <Refine
+                  routerProvider={routerBindings}
+                  dataProvider={dataProvider(
+                      "https://api.fake-rest.refine.dev",
+                  )}
+                  notificationProvider={notificationProvider}
+                  resources={[
+                      {
+                          name: "blog_posts",
+                          list: "/blog-posts",
+                          show: "/blog-posts/show/:id",
+                          create: "/blog-posts/create",
+                          edit: "/blog-posts/edit/:id",
+                      },
+                  ]}
+                  options={{
+                      syncWithLocation: true,
+                      warnWhenUnsavedChanges: true,
+                  }}
+              >
+                  <Routes>
+                      <Route
+                          element={
+                              <ThemedLayoutV2>
+                                  <Outlet />
+                              </ThemedLayoutV2>
+                          }
+                      >
+                          <Route
+                              index
+                              element={
+                                  <NavigateToResource resource="blog_posts" />
+                              }
+                          />
+                          <Route path="blog-posts">
+                              <Route index element={<BlogPostList />} />
+                              <Route
+                                  path="show/:id"
+                                  element={<BlogPostShow />}
+                              />
+                              <Route
+                                  path="create"
+                                  element={<BlogPostCreate />}
+                              />
+                              <Route
+                                  path="edit/:id"
+                                  element={<BlogPostEdit />}
+                              />
+                          </Route>
 
+                          <Route path="*" element={<ErrorComponent />} />
+                      </Route>
+                  </Routes>
+                  <UnsavedChangesNotifier />
+              </Refine>
+          </ConfigProvider>
+      </BrowserRouter>
+  );
+};
 export default App;
